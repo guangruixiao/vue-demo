f9a7afec83d9af8c6cc3620f49ff39e0c948ca9b
guangruixiao <1245420476@qq.com>
2017-02-22 20:08:00 +0800

    Bootstap+Vue+Flask

---
+++ todolist/todolist.py
@@ -0,0 +1,26 @@
+
+#-*-coding:utf-8-*-
+
+
+#初始化
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+
+
+
+#路由和视图函数
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/data")
+def test():
+	return "<p>呵呵，后台暂时没有内容，不过这条数据来自后台!</p>"
+
+
+
+#启动服务器
+if __name__=="__main__":
+	app.run(debug=True)
